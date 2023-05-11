4566eaf363351888bcdff1d6c92b172eeadbab7c
engrceo <ceokereke@yahoo.com>
2023-05-12 00:35:48 +0800

    new

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-import flask
-import dash
-from dash import html
+from dash import Dash, dcc, html, Input, Output
+import os
 
 
-server = flask.Flask(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-@server.route('/')
-def index():
-    return 'Hello Flask app'
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-app = dash.Dash(
-    __name__,
-    server=server,
-    routes_pathname_prefix='/dash/'
-)
+server = app.server
 
-app.layout = html.Div("My Dash app")
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
